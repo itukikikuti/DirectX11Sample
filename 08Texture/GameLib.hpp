f5caf5c98b0856cb8d96821611a6d65e8c7860d3
菊池いつき <xxvraqwa@gmail.com>
2018-12-04 18:11:54 +0900

    Texture完成

---
+++ 08Texture/GameLib.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#define OEMRESOURCE
+#include <Windows.h>
+#include <DirectXMath.h>
+#include <d3d11.h>
+#include <DirectXMath.h>
+#include <wrl.h>
+#include <memory>
+#include <d3d11.h>
+#include <wrl.h>
+#include <vector>
+#include <d3d11.h>
+#include <wincodec.h>
+#include <wrl.h>
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "windowscodecs.lib")
